2853c175517c49beb0b4481cddbe12cc7eebdfca
Shane Barringer <shane.barringer@outlook.com>
2016-05-16 18:24:26 -0400

    adds initial files, configures user and tweet models

---
+++ db/migrate/20160511235838_add_attributes_to_user.rb
@@ -0,0 +1,8 @@
+class AddAttributesToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :name, :string
+    add_column :users, :username, :string
+    add_column :users, :bio, :text
+    add_column :users, :location, :string
+  end
+end
